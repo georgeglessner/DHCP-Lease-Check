ea00c57a94984b3177ab4edb271c19a230349c6b
georgeglessner <g.glessner@outlook.com>
2019-11-21 09:05:17 -0700

    Update lease list format

--- leaseCheck.py
+++ leaseCheck.py
@@ -9,66 +9,67 @@ import os
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(SERVER, username=USER, password=PASSWORD)
-stdin, stdout, ssh_stderr = ssh.exec_command('cat /tmp/dnsmasq.leases')
+stdin, stdout, ssh_stderr = ssh.exec_command("cat /tmp/dnsmasq.leases")
 
 # get current leases from ssh command
-leases = stdout.read().decode('utf-8')
+leases = stdout.read().decode("utf-8")
 stdin.flush()
 ssh.close()
 
 # check to see if leaseList.txt exists
 # create file if it doesn't
-if not os.path.exists('./leaseList.txt'):
-    open('leaseList.txt', 'a').close()
+if not os.path.exists("./leaseList.txt"):
+    open("leaseList.txt", "a").close()
 
 # initialize current leases array
 currentLeases = []
 
 # get current leases MAC addresses
 # add to currentLeases array
-for lease in leases.split('\n'):
+for lease in leases.split("\n"):
     if len(lease) > 0:
-        leaseCheck = re.compile('(?:[0-9a-fA-F]:?){12}')
+        leaseCheck = re.compile("(?:[0-9a-fA-F]:?){12}")
         activeLeases = re.findall(leaseCheck, lease)
-        currentLeases.append(activeLeases[0])
+        currentLeases.append("{} : {}".format(activeLeases[0], lease.split()[3]))
 
 # add current leases to leaseList.txt if empty
-if os.stat('leaseList.txt').st_size == 0:
-    with open('leaseList.txt', 'w') as w:
+if os.stat("leaseList.txt").st_size == 0:
+    with open("leaseList.txt", "w") as w:
         for lease in currentLeases:
             if len(lease) > 0:
-                w.write('{}\n'.format(lease))
+                w.write("{}\n".format(lease))
     w.close()
 
 # create lease list array from leaseList.txt
-leaseList = [line.rstrip('\n') for line in open('leaseList.txt')]
+leaseList = [line.rstrip("\n") for line in open("leaseList.txt")]
 
 # clear out the current lease list
-with open('leaseList.txt', 'w'):
+with open("leaseList.txt", "w"):
     pass
 
 
 # find any new leases, add to array
 # add new leases to leaseList.txt
 newLeases = []
-with open('leaseList.txt', 'w') as f:
+with open("leaseList.txt", "w") as f:
     for lease in currentLeases:
-        f.write('{}\n'.format(lease))
+        f.write("{}\n".format(lease))
         if (len(lease) > 0) and (lease not in leaseList):
             newLeases.append(lease)
 f.close()
+
 
 # send e-mail with new leases
 if len(newLeases) > 0:
     # set up message
     msg = EmailMessage()
-    msg['Subject'] = 'New DHCP Leases Found'
-    msg['From'] = G_USER
-    msg['To'] = G_TO
+    msg["Subject"] = "New DHCP Leases Found"
+    msg["From"] = G_USER
+    msg["To"] = G_TO
     msg.set_content(str(newLeases))
 
     # send message
-    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s = smtplib.SMTP("smtp.gmail.com", 587)
     s.starttls()
     s.login(G_USER, G_PASS)
     s.send_message(msg)
